0c124b55139a1bb7696b79c84f52a26449f34b5e
danulyk05 <ivan.danulyk2016@gmail.com>
2022-07-30 21:26:43 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Louis_Vuitton_logo_and_wordmark.svg/630px-Louis_Vuitton_logo_and_wordmark.svg.png"></img>
+    </header>
+  );
+};
+
+export default Header;
