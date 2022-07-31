8dd044a00a6216988cac7b54e08c3d01c0be2e8d
danulyk05 <ivan.danulyk2016@gmail.com>
2022-07-31 14:17:18 +0300

    14-css-module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import c from './Header.module.css';
+
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={c.header}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Louis_Vuitton_logo_and_wordmark.svg/630px-Louis_Vuitton_logo_and_wordmark.svg.png"></img>
     </header>
   );
