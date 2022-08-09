da5483ef2436324651d70f6ff1d091ea8505de5b
danulyk05 <ivan.danulyk2016@gmail.com>
2022-08-09 13:32:02 +0300

    react after grid changes

--- src/components/Profile/MyPost/Post/Post.jsx
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import s from "./Post.module.css";
 
 const Post = () => {
-  return <div className={s.item}>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpfKtkhWqckmOVvubt3aZfCnxMip6KY8lbX5o624GJY-3nMzq0exyVbWbDdgEe6Gr7kow&usqp=CAU" />
-    post 1
-    </div>;
+  return (
+    <div className={s.item}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpfKtkhWqckmOVvubt3aZfCnxMip6KY8lbX5o624GJY-3nMzq0exyVbWbDdgEe6Gr7kow&usqp=CAU" />
+      post 1
+      <div>
+        <span>like</span>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
